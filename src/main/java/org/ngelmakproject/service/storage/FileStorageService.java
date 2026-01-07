4d761f420e21942121006f1190d08125e68f2731
Youssouph Faye <youssouph.faye@gmail.com>
2026-01-07 19:52:52 +0100

    Fast update

--- src/main/java/org/ngelmakproject/service/storage/FileStorageService.java
+++ src/main/java/org/ngelmakproject/service/storage/FileStorageService.java
@@ -28,11 +28,11 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FileStorageService {
 
-  @Value("${nk.file.upload-directory.location}")
+  @Value("${file.upload-directory.location}")
   private String location;
-  @Value("${nk.file.public.access.location}")
+  @Value("${file.public.access.location}")
   private String publicAccessLocation;
-  @Value("${nk.file.private.access.location}")
+  @Value("${file.private.access.location}")
   private String privateAccessLocation;
 
   @Value("${server.host}")
