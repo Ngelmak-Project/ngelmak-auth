99e0b0c483132b1cc9a981ea3765c81563fb63d2
Youssouph Faye <youssouph.faye@gmail.com>
2026-02-11 12:31:05 +0100

    Fast update

--- src/main/java/org/ngelmakproject/web/rest/dto/AuthorityRequestDTO.java
+++ src/main/java/org/ngelmakproject/web/rest/dto/AuthorityRequestDTO.java
@@ -2,19 +2,18 @@ package org.ngelmakproject.web.rest.dto;
 
 import java.time.Instant;
 
-import org.ngelmakproject.domain.Authority;
 import org.ngelmakproject.domain.AuthorityRequest;
 import org.ngelmakproject.domain.AuthorityRequest.RequestStatus;
 
 public record AuthorityRequestDTO(Long id,
-        Authority authority,
+        String authority,
         String motivation,
         RequestStatus status,
         Instant requestedAt) {
     public static AuthorityRequestDTO from(AuthorityRequest authorityRequest) {
         return new AuthorityRequestDTO(
                 authorityRequest.getId(),
-                authorityRequest.getAuthority(),
+                authorityRequest.getAuthority().getName(),
                 authorityRequest.getMotivation(),
                 authorityRequest.getStatus(),
                 authorityRequest.getRequestedAt());
